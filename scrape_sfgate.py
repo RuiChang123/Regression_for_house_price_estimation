224440aef23b4d036f6141b580a5bffa4de5db22
Rui Chang <changrui.c@gmail.com>
2016-04-20 15:12:04 -0700

    code update

--- scrape_sfgate.py
+++ scrape_sfgate.py
@@ -23,7 +23,7 @@ app = QApplication(sys.argv)
 
 for i in range(1,13): 
     #url = """http://www.sfgate.com/webdb/homesales/?appSession=62394121042939095974604243804125972115667225306241842463756579389539619617572902719647069617948654518042925562712294167607160157&RecordID=&PageID=2&PrevPageID=1&cpipage=""" +str(i)+'&CPISortType=&CPIorderBy='
-    #url = 'http://www.sfgate.com/webdb/homesales/?appSession=95321263662334992584489562332295501542748135578959443742621858893452432463029521137221863603304156826975461352994264703512194626&RecordID=&PageID=2&PrevPageID=1&cpipage='+str(i)+'&CPISortType=&CPIorderBy='
+    #the url changes everyday
     url = 'http://www.sfgate.com/webdb/homesales/?appSession=64329322129873111204574554973483280103329065656847042053181216317168570306162401356813312547725230111651362605283184354571771674&RecordID=&PageID=2&PrevPageID=1&cpipage='+str(i)+'&CPISortType=&CPIorderBy='
 
     html = loadPage(url)
